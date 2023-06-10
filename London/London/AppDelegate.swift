3437c7d64a00597f502dbfdc1513320ad3c7c45e
Ugur Polat <ugurpolat113@gmail.com>
2023-06-10 09:57:51 +0300

    Add tableview

--- London/London/AppDelegate.swift
+++ London/London/AppDelegate.swift
@@ -28,7 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = mainViewController
+        //window?.rootViewController = mainViewController
+        window?.rootViewController = AccountSummaryViewController()
         
         return true
     }
