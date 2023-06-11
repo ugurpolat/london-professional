e2e883ac61267e8094aadc4480ad78c9fb06f5ef
Ugur Polat <ugurpolat113@gmail.com>
2023-06-11 22:21:53 +0300

    Add viewModel for cell. Add currecy formatter

---
+++ London/London/Utils/CurrencyFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyFormatter.swift
+//  London
+//
+//  Created by Ugur Polat on 6/11/23.
+//
+
+import Foundation
---
+++ London/London/Utils/DecimalUtils.swift
@@ -0,0 +1,8 @@
+//
+//  DecimalUtils.swift
+//  London
+//
+//  Created by Ugur Polat on 6/11/23.
+//
+
+import Foundation
