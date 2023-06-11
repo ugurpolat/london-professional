fa19c886408015a452c18448ab6b5f8cbecba4c6
Ugur Polat <ugurpolat113@gmail.com>
2023-06-11 14:00:37 +0300

    Add cell&style cell

--- London/London/AccountSummary/AccountSummaryViewController.swift
+++ London/London/AccountSummary/AccountSummaryViewController.swift
@@ -12,6 +12,15 @@ class AccountSummaryViewController: UIViewController {
             "Pacman",
             "Space Invaders",
             "Space Patrol",
+            "Pacman",
+            "Space Invaders",
+            "Space Patrol",
+            "Pacman",
+            "Space Invaders",
+            "Space Patrol",
+            "Pacman",
+            "Space Invaders",
+            "Space Patrol"
         ]
         
         var tableView = UITableView()
@@ -31,6 +40,10 @@ class AccountSummaryViewController: UIViewController {
         private func setupTableView() {
             tableView.delegate = self
             tableView.dataSource = self
+            
+            tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
+            tableView.rowHeight = AccountSummaryCell.rowHeight
+            tableView.tableFooterView = UIView()
             
             tableView.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview(tableView)
@@ -56,8 +69,7 @@ class AccountSummaryViewController: UIViewController {
 
     extension AccountSummaryViewController: UITableViewDataSource {
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = UITableViewCell()
-            cell.textLabel?.text = games[indexPath.row]
+            let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
             return cell
         }
         
